9fc5ba8a6cdffb669385fc3c27b011a261269534
Rifayat Quadir <96713174+RifayatQ@users.noreply.github.com>
2023-08-29 22:38:45 -0600

    Add cusineEDA.py

---
+++ cusineEDA.py
@@ -0,0 +1,27 @@
+#####################
+# Welcome to Cursor #
+#####################
+
+'''
+Step 1: Try generating with Cmd+K or Ctrl+K on a new line. Ask for CLI-based game of TicTacToe.
+
+Step 2: Hit Cmd+L or Ctrl+L and ask the chat what the code does. 
+   - Then, try running the code
+
+Step 3: Try highlighting all the code with your mouse, then hit Cmd+k or Ctrl+K. 
+   - Instruct it to change the game in some way (e.g. add colors, add a start screen, make it 4x4 instead of 3x3)
+
+Step 4: To try out cursor on your own projects, go to the file menu (top left) and open a folder.
+'''
+##Data Science project on automating the process of determining the cuisine of a food recipe
+# download library to read data into dataframe
+import pandas as pd 
+pd.set_option('display.max_columns', None)
+
+# Download data from IBM server and read into pandas dataframe
+recipes = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0103EN-SkillsNetwork/labs/Module%202/recipes.csv")
+
+# check first few rows
+print(recipes.head()) 
+# get dimension of the data frame
+# print(recipes.shape)  
