4e57d43f8afcbf51f832ecd63a7528fc6dd12cd3
Rifayat Quadir <96713174+RifayatQ@users.noreply.github.com>
2023-08-29 22:52:59 -0600

    Update cusineEDA.py

--- cusineEDA.py
+++ cusineEDA.py
@@ -1,18 +1,3 @@
-#####################
-# Welcome to Cursor #
-#####################
-
-'''
-Step 1: Try generating with Cmd+K or Ctrl+K on a new line. Ask for CLI-based game of TicTacToe.
-
-Step 2: Hit Cmd+L or Ctrl+L and ask the chat what the code does. 
-   - Then, try running the code
-
-Step 3: Try highlighting all the code with your mouse, then hit Cmd+k or Ctrl+K. 
-   - Instruct it to change the game in some way (e.g. add colors, add a start screen, make it 4x4 instead of 3x3)
-
-Step 4: To try out cursor on your own projects, go to the file menu (top left) and open a folder.
-'''
 ##Data Science project on automating the process of determining the cuisine of a food recipe
 # download library to read data into dataframe
 import pandas as pd 
@@ -22,6 +7,15 @@ pd.set_option('display.max_columns', None)
 recipes = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0103EN-SkillsNetwork/labs/Module%202/recipes.csv")
 
 # check first few rows
-print(recipes.head()) 
+# print(recipes.head()) 
 # get dimension of the data frame
-# print(recipes.shape)  
+# print(recipes.shape) 
+
+# the dataset consists of 57,961 recipes. From initial EDA, each row represents a recipe, and for each recipe, the cuisine is documented as well as whether 384 ingredients exist in the recipe or not, beginning with almond ending with zucchini.
+
+# checking if the ingredients of a sushi (i.e., rice, soy sauce, wasabi, fish/vegetables) exist in dataframe
+ingredients = list(recipes.columns.values) 
+
+print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(rice).*")).search(ingredient)] if match])
+print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(wasabi).*")).search(ingredient)] if match])
+
